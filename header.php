36a60b87ce3b77af6a10039f53d7b77cf5dab829
Nikki Beesetti <nbeesett@purdue.edu>
2015-07-12 23:26:45 -0500

    Create header.php
    Header for the website

---
+++ header.php
@@ -0,0 +1,147 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
+<style>
+body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Oxygen', Arial, Helvetica, sans-serif;
+    color: #545454;
+}
+a {
+    color: #1e5799;
+    text-decoration: none !important;
+}
+a:hover {
+    color: #7db9e8;
+}
+.wrapper {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 0 10px;
+}
+.banner { 
+    display: block; 
+    margin: 0 auto 50px; 
+    max-width: 100%;
+}
+header { 
+    /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#1e5799+0,7db9e8+100 */
+    background: #1e5799; /* Old browsers */
+    background: -moz-linear-gradient(top,  #1e5799 0%, #7db9e8 100%); /* FF3.6+ */
+    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#1e5799), color-stop(100%,#7db9e8)); /* Chrome,Safari4+ */
+    background: -webkit-linear-gradient(top,  #1e5799 0%,#7db9e8 100%); /* Chrome10+,Safari5.1+ */
+    background: -o-linear-gradient(top,  #1e5799 0%,#7db9e8 100%); /* Opera 11.10+ */
+    background: -ms-linear-gradient(top,  #1e5799 0%,#7db9e8 100%); /* IE10+ */
+    background: linear-gradient(to bottom,  #1e5799 0%,#7db9e8 100%); /* W3C */
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#1e5799', endColorstr='#7db9e8',GradientType=0 ); /* IE6-9 */
+}
+header h1 {
+    font-family: 'Open Sans Condensed', Arial, Helvetica, sans-serif;
+    font-size: 80px;
+    color: #fff;
+    text-align: center;
+    padding: 70px 0;
+    margin: 0;
+    text-transform: uppercase;
+}
+header h1 a {
+    color: #fff !important;
+}
+.intro {
+    background: #ddd;
+}
+.intro p {
+    font-size: 30px;
+    padding: 40px 0;
+}
+.description p {
+    font-size: 18px;
+    margin: 40px 0;
+}
+.search {
+    background: #7db9e8;
+    padding: 40px 0;
+}
+.search p {
+    font-size: 20px;
+    color: #fff;
+}
+#search { 
+    margin: 0;
+}
+#search input[type="text"] {
+    padding: 10px;
+    border: 1px solid #ddd;
+    width: 70%;
+}
+#search input[type="submit"], .button {
+    color: #fff !important;
+    padding: 10px 20px;
+    background: #1e5799;
+    border: 1px solid #1e5799;
+    display: inline-block;
+}
+#search input[type="submit"]:hover, .button:hover {
+    background: #5b97c6;
+    border: 1px solid #5b97c6;
+}
+#search select {
+    padding: 10px;
+    border: 1px solid #ddd;
+}
+.donate {
+    padding: 40px 10px;
+    background: #ddd;
+}
+.donate .wrapper {
+    margin: 0 auto 20px;
+    font-size: 20px;
+}
+.donate span {
+    vertical-align: middle;
+    display: inline-block;
+    margin-right: 10px;
+}
+.donate .button {
+    font-size: 20px;
+    padding: 10px 30px;
+    vertical-align: middle;
+}
+.results {
+    padding: 40px 0;
+}
+.results p {
+    line-height: 1.5;
+    font-size: 16px;
+}
+.bottom {
+    padding: 20px 0 50px;
+}
+.charity-heading, .results h2 {
+    font-size: 30px;
+}
+.logo {
+    height: 90px;
+    margin-bottom: 50px;
+}
+.image {
+    display: inline-block;
+    vertical-align: top;
+    width: 15%;
+    margin-right: 5%;
+    margin-top: 25px;
+}
+.image .logo {
+    height: auto;
+    width: 100%;
+}
+.charity-blurb {
+    display: inline-block;
+    vertical-align: top;
+    width: 80%;
+}
+</style>
